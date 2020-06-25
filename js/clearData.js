a95ec0832e79e2d08d4ee650ea4f8bbd18e26047
iScherzo <57310720+iScherzo@users.noreply.github.com>
2020-06-25 19:29:14 +0700

    add Clear Data

---
+++ js/clearData.js
@@ -0,0 +1,28 @@
+function clearAllInput() {
+    var printResult = document.getElementById("showdata");
+    var printStep = document.getElementById("stepCalculating");
+    var rowValue = document.getElementById("rowValue");
+    var rowValKet = document.getElementById("rowValKet");
+    var weightValue = document.getElementById("weightValue");
+
+
+    while (printResult.hasChildNodes()) {
+        printResult.removeChild(printResult.lastChild);
+    }
+
+    while (printStep.hasChildNodes()) {
+        printStep.removeChild(printStep.lastChild);
+    }
+
+    while (rowValue.hasChildNodes()) {
+        rowValue.removeChild(rowValue.lastChild);
+    }
+
+    while (rowValKet.hasChildNodes()) {
+        rowValKet.removeChild(rowValKet.lastChild);
+    }
+
+    while (weightValue.hasChildNodes()) {
+        weightValue.removeChild(weightValue.lastChild);
+    }
+}
